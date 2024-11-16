0abd547d28784c37b04063943a4f9aae49c49041
Olya <olgariabuxa0@gmail.com>
2024-11-16 23:53:24 +0200

    add hw12

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = '47106635-e689f78aea9055419384fac8a';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchImages(query, page) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: page,
+      per_page: 15,
+    },
+  });
+  return response.data.hits;
+}
